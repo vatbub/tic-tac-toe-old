b3a4bbe17d09b174ffd8d3f53f215a1c1e638d04
ga59tim <frederik.kammel@tum.de>
2015-12-10 12:08:44 +0100

    First setup of an ai

---
+++ tic-tac-toe/TicTacToe/src/ai/MiniMax.java
@@ -0,0 +1,14 @@
+package ai;
+
+public class MiniMax {
+
+	private tree.Node<AiGameTable> gameTree;
+	
+	/**
+	 * Decides where the AI will do its next turn by implementing the Mini-Max-Algorithm
+	 * @return The position where the AI will play.
+	 */
+	public static int[] doAiTurn(){
+		return null;
+	}
+}
