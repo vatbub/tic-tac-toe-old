c6e34c8e442b8e9d89ee213419ae6f53d13d3086
vatbub <vatbub123+github@googlemail.com>
2015-12-10 18:46:58 +0100

    Moved MiniMax Algorithm to Player Class and implemented it there (although not running yet)

--- tic-tac-toe/TicTacToe/src/model/Player.java
+++ tic-tac-toe/TicTacToe/src/model/Player.java
@@ -6,7 +6,11 @@
 
 package model;
 
+import java.util.ArrayList;
+
 import javax.swing.JOptionPane;
+
+import gui.GameJTable;
 
 public class Player {
 	public static Player Player1;
@@ -15,10 +19,17 @@ public class Player {
 	public static int playerCount;
 
 	public String name;
+	
+	/**
+	 * Specifies if the player is an artificial intelligence (true) or a human (false)
+	 */
+	public boolean isAi; 
 
 	/**
 	 * Initializes a new Player.
-	 * @param name Name of the Player
+	 * 
+	 * @param name
+	 *            Name of the Player
 	 */
 	public Player(String name) {
 		playerCount = playerCount + 1;
@@ -29,22 +40,24 @@ public class Player {
 			this.name = name;
 		}
 	}
-	
+
 	/**
 	 * Switches Player.Player1 and Player.player2
 	 */
-	public static void switchPlayers(){
-		//Switches Player1 and Player 2
-		Player player1Copy=Player1;
-		Player player2Copy=Player2;
-		
-		Player2=player1Copy;
-		Player1=player2Copy;
+	public static void switchPlayers() {
+		// Switches Player1 and Player 2
+		Player player1Copy = Player1;
+		Player player2Copy = Player2;
+
+		Player2 = player1Copy;
+		Player1 = player2Copy;
 	}
 
 	/**
 	 * Guides the user through the name selection.
-	 * @return Returns true, if all dialogs were completed successfully and false if the user cancelled the operation
+	 * 
+	 * @return Returns true, if all dialogs were completed successfully and
+	 *         false if the user cancelled the operation
 	 */
 	public static boolean initPlayers() {
 		// returns false if the player wishes to cancel the game
@@ -54,17 +67,21 @@ public class Player {
 
 		// Ask for Player 1's name
 		String namePlayer1;
-		
-		if (Player1==null){
-			namePlayer1="Player " + (playerCount + 1);
-		} else if (Player1.name==""){
-			namePlayer1="Player " + (playerCount + 1);
-		}else{
-			namePlayer1=Player1.name;
+
+		if (Player1 == null) {
+			namePlayer1 = "Player " + (playerCount + 1);
+		} else if (Player1.name == "") {
+			namePlayer1 = "Player " + (playerCount + 1);
+		} else {
+			namePlayer1 = Player1.name;
 		}
-		
-		// String namePlayer2=(String)JOptionPane.showInputDialog(null, "Message", "Title", JOptionPane.INFORMATION_MESSAGE, null, null, "DefaultValue");
-		namePlayer1=(String)JOptionPane.showInputDialog(null, "Player " + (playerCount + 1) + ", please enter your name", "Playernames", JOptionPane.INFORMATION_MESSAGE, null, null, namePlayer1);
+
+		// String namePlayer2=(String)JOptionPane.showInputDialog(null,
+		// "Message", "Title", JOptionPane.INFORMATION_MESSAGE, null, null,
+		// "DefaultValue");
+		namePlayer1 = (String) JOptionPane.showInputDialog(null,
+				"Player " + (playerCount + 1) + ", please enter your name", "Playernames",
+				JOptionPane.INFORMATION_MESSAGE, null, null, namePlayer1);
 
 		// Check if the player clicked Cancel
 		if (namePlayer1 == null) {
@@ -75,16 +92,20 @@ public class Player {
 
 		// Ask for Player 2's name
 		String namePlayer2;
-		
-		if (Player2==null){
-			namePlayer2="Player " + (playerCount + 1);
-		} else if (Player2.name==""){
-			namePlayer2="Player " + (playerCount + 1);
-		}else{
-			namePlayer2=Player2.name;
+
+		if (Player2 == null) {
+			namePlayer2 = "Player " + (playerCount + 1);
+		} else if (Player2.name == "") {
+			namePlayer2 = "Player " + (playerCount + 1);
+		} else {
+			namePlayer2 = Player2.name;
 		}
-		// String namePlayer2=(String)JOptionPane.showInputDialog(null, "Message", "Title", JOptionPane.INFORMATION_MESSAGE, null, null, "DefaultValue");
-		namePlayer2=(String)JOptionPane.showInputDialog(null, "Player " + (playerCount + 1) + ", please enter your name", "Playernames", JOptionPane.INFORMATION_MESSAGE, null, null, namePlayer2);
+		// String namePlayer2=(String)JOptionPane.showInputDialog(null,
+		// "Message", "Title", JOptionPane.INFORMATION_MESSAGE, null, null,
+		// "DefaultValue");
+		namePlayer2 = (String) JOptionPane.showInputDialog(null,
+				"Player " + (playerCount + 1) + ", please enter your name", "Playernames",
+				JOptionPane.INFORMATION_MESSAGE, null, null, namePlayer2);
 
 		// Check if the player clicked Cancel
 		if (namePlayer2 == null) {
@@ -94,7 +115,93 @@ public class Player {
 		Player2 = new Player(namePlayer2);
 
 		// We only arrive here if no cancel button was clicked
-		PlayerTie=new Player("tie");
+		PlayerTie = new Player("tie");
 		return true;
+	}
+
+	/**
+	 * Decides where the AI will do its next turn by implementing the
+	 * Mini-Max-Algorithm
+	 * 
+	 * @return The position where the AI will play.
+	 */
+	public void doAiTurn(GameJTable currentGameTable) {
+		TreeNode gameTree = buildGameTree((AiGameTable)currentGameTable);
+		int bestTurn = gameTree.getBestTurnFromChildren();
+		
+		//do the actual turn
+		currentGameTable.playerPlayed(gameTree.getChildAt(bestTurn).getObject().playedAtRow, gameTree.getChildAt(bestTurn).getObject().playedAtColumn, this);
+	}
+	
+	/**
+	 * Builds a tree with all possible turn combinations
+	 * @param currentGameTable The current gameTable
+	 * @return The tree containing all possible turn combinations
+	 */
+	private TreeNode buildGameTree(AiGameTable currentGameTable){
+		return buildGameTree_recursive(currentGameTable,false);
+	}
+
+	/**
+	 * Builds a tree with all possible turn combinations
+	 * @param currentGameTable The current gameTable
+	 * @param opponentsTurn Specifies if scores should be inverted in this part of the tree becase its the opponents turn
+	 * @return The tree containing all possible turn combinations
+	 */
+	private TreeNode buildGameTree_recursive(AiGameTable currentGameTable, boolean opponentsTurn) {
+		TreeNode gameTree = new TreeNode();
+
+		int scoreCoeff = 1;
+
+		// invert scores eventually
+		if (opponentsTurn == true) {
+			scoreCoeff = -1;
+		}
+
+		// determine all possible turns
+		ArrayList<int[]> turns;
+		turns = new ArrayList<int[]>();
+
+		for (int r = 0; r < currentGameTable.getRowCount(); r++) {
+			for (int c = 0; c < currentGameTable.getColumnCount(); c++) {
+				if (currentGameTable.getPlayerAt(r, c) == null) {
+					int[] rc = { r, c };
+					turns.add(rc);
+				}
+			}
+		}
+
+		gameTree.setObject(currentGameTable);
+
+		for (int i = 0; i < turns.size(); i++) {
+			// duplicate the table
+			AiGameTable tableTemp = gameTree.getObject().clone();
+
+			// do the turn
+			tableTemp.setPlayerAt(turns.get(i)[0], turns.get(i)[1], this);
+
+			// check if somebody won
+			Player playerWonTemp = tableTemp.winDetector2(turns.get(i)[0], turns.get(i)[1]);
+
+			// set the score to 10 if I would win and to -10 if the opponent
+			// would win and continue if nobody would win
+			if (playerWonTemp == null) {
+				// opponents turn
+				gameTree.addChild(buildGameTree_recursive(tableTemp, !opponentsTurn));
+			} else {
+				if (playerWonTemp.equals(this)) {
+					tableTemp.scoreIfStateIsReached = 10 * scoreCoeff;
+				} else if (playerWonTemp.equals(PlayerTie)) {
+					//its a tie
+					tableTemp.scoreIfStateIsReached = 5 * scoreCoeff;
+				} else {
+					// opponent wins
+					tableTemp.scoreIfStateIsReached = -10 * scoreCoeff;
+				}
+				gameTree.addChild(new TreeNode(tableTemp));
+			}
+		}
+
+		return gameTree;
 	}
 }
