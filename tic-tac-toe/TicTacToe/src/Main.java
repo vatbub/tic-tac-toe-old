9391aa447473313ba84deabed384e5c1bf4f9171
ga59tim <frederik.kammel@tum.de>
2015-12-13 21:52:10 +0100

    Fixed the Javadoc not showing the class description

--- tic-tac-toe/TicTacToe/src/Main.java
+++ tic-tac-toe/TicTacToe/src/Main.java
@@ -1,9 +1,16 @@
+/**
+ * Starts the game
+ * 
+ * @author Frederik Kammel
+ */
 public class Main {
 
 	/**
 	 * Starts the game
+	 * 
 	 * @author Frederik Kammel
-	 * @param args Commandline arguments (are ignored currently)
+	 * @param args
+	 *            Commandline arguments (are ignored currently)
 	 */
 	public static void main(String[] args) {
 		// Show the welcome GUI
